2cc62f0c3d9cd3395b5509bf0976f0fd94eb84a7
cod1r <jasonho353@gmail.com>
2021-12-20 04:36:12 -0600

    changed api to look for the correct parameters in the body object so that the rooms query will work properly and return the query results

--- pages/api/getrooms.ts
+++ pages/api/getrooms.ts
@@ -1,9 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { pool } from '../../database/databaseinit';
+
+// the way I set up getrooms and getusers is a little wonky since getusers also 
+// returns search results as well as friends for the user and how I know whether to return
+// search results or friends is with the querytype parameter in the json passed from 
+// the client to the server.
 export default function getrooms(req: NextApiRequest, res: NextApiResponse) {
 	let body = JSON.parse(req.body);
-	if ('query' in body) {
-		pool.query("SELECT ROOMNAME FROM ROOMS WHERE ROOMNAME LIKE ?", ['%' + body.query + '%'], (err, results, fields) => {
+	if ('search' in body) {
+		pool.query("SELECT ROOMNAME FROM ROOMS WHERE ROOMNAME LIKE ?", ['%' + body.search + '%'], (err, results, fields) => {
 			if (err) {
 				console.log(err);
 				return;
