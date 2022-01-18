0a27904a88f8532a14d6b372964efd69a59b9465
cod1r <jasonho353@gmail.com>
2022-01-17 18:39:30 -0600

    userinfo api file had some code that we aren't using anymore

--- pages/api/userinfo.ts
+++ pages/api/userinfo.ts
@@ -3,15 +3,4 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { client } from '../../database/databaseinit';
 
 export default function userinfo(req: NextApiRequest, res: NextApiResponse) {
-	let body = JSON.parse(req.body);
-	let cookies = req.cookies;
-	let token = cookies['rememberme'];
-	jwt.verify(token, process.env.private_key, (err, decoded) => {
-		res.send({
-			username: results[0][0]['USERNAME'],
-			bio: results[0][0]['BIO']?.toString('utf8'),
-			isFriend: results[1][0]['COUNT(USERNAME)'] === 1
-				|| body.username === decoded.username,
-		});
-	});
 }
