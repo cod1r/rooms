e879a84d4baef3622c8f8d67a1929e9751bc838b
cod1r <jasonho353@gmail.com>
2022-01-17 18:35:28 -0600

    changed environment vars because vercel has some vars reserved

--- database/databaseinit.ts
+++ database/databaseinit.ts
@@ -2,8 +2,8 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 let client = new DynamoDBClient({
 	credentials: {
-		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID_ROOMS,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ROOMS,
 	},
 	region: 'us-east-1',
 });
